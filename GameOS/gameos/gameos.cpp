d3f75841b5c9cd696faf34002b3d0c5aa0c7ab2d
sebi <alariq@gmail.com>
2016-09-25 23:14:21 +0300

    basic stub implementation for Texture functions

--- GameOS/gameos/gameos.cpp
+++ GameOS/gameos/gameos.cpp
@@ -5,6 +5,105 @@
 #include <time.h>
 #include <stdlib.h> // rand
 #include "strres.h"
+#include <vector>
+
+struct gosTextureInfo {
+    int width_;
+    int height_;
+    gos_TextureFormat format_;
+};
+
+class gosTexture {
+    public:
+        gosTexture(gos_TextureFormat fmt, const char* fname, DWORD hints, BYTE* pdata, DWORD size)
+        {
+            format_ = fmt;
+            if(fname) {
+                filename_ = new char[strlen(fname)+1];
+                strcpy(filename_, fname);
+            } else {
+                filename_ = 0;
+            }
+            hints_ = hints;
+            
+            size_ = size;
+            pdata_ = new BYTE[size];
+            memcpy(pdata_, pdata, size);
+
+            is_locked_ = false;
+
+            // TODO: correctly set width and height
+            width_ = 16;
+            height_ = 16;
+            
+        }
+        ~gosTexture() {
+            delete[] pdata_;
+            delete[] filename_;
+        }
+
+        BYTE* Lock(int mipl_level, float is_read_only, int* pitch) {
+            gosASSERT(is_locked_ == false);
+            is_locked_ = true;
+            // TODO:
+            gosASSERT(pitch);
+            *pitch = width_;
+            return pdata_;
+        }
+
+        void Unlock() {
+            gosASSERT(is_locked_ == true);
+            is_locked_ = false;
+        }
+
+        void getTextureInfo(gosTextureInfo* texinfo) {
+            gosASSERT(texinfo);
+            texinfo->width_ = width_;
+            texinfo->height_ = height_;
+            texinfo->format_ = format_;
+        }
+
+    private:
+        int width_;
+        int height_;
+
+        BYTE* pdata_;
+        DWORD size_;
+
+        gos_TextureFormat format_;
+        char* filename_;
+        DWORD hints_;
+
+        bool is_locked_;
+};
+
+class gosRenderer {
+    public:
+        DWORD addTexture(gosTexture* texture) {
+            gosASSERT(texture);
+            textureList_.push_back(texture);
+            return textureList_.size()-1;
+        }
+
+        gosTexture* getTexture(DWORD texture_id) {
+            gosASSERT(textureList_.size() > texture_id);
+            gosASSERT(textureList_[texture_id] != 0);
+            return textureList_[texture_id];
+        }
+
+        void deleteTexture(DWORD texture_id) {
+            // FIXME: bad use object list, with stable ids
+            // to not waste space
+            gosASSERT(textureList_.size() > texture_id);
+            delete textureList_[texture_id];
+            textureList_[texture_id] = 0;
+        }
+
+    private:
+        std::vector<gosTexture*> textureList_;
+};
+
+static gosRenderer g_renderer;
 
 ////////////////////////////////////////////////////////////////////////////////
 void AddDebuggerMenuItem(char const*, bool (*)(), void (*)(), bool (*)(), DWORD (*)(char const*, DWORD))
@@ -61,7 +160,7 @@ __int64 __stdcall GetCycles()
 //
 void __stdcall gosAudio_CreateResource( HGOSAUDIO* hgosaudio, enum gosAudio_ResourceType,  const char* file_name, gosAudio_Format* ga_wf, void* data, int size, bool only2D)
 {
-
+    
 }
 
 //////////////////////////////////////////////////////////////////////////////////
@@ -197,8 +296,13 @@ void __stdcall gos_CloseResourceDLL(HSTRRES handle)
 }
 const char* __stdcall gos_GetResourceString(HSTRRES handle, DWORD id)
 {
+    static const char* dummy_string_res = "missing string res";
     gosASSERT(handle);
     gos_StringRes* pstrres = (gos_StringRes*)(handle);
+    if(id > pstrres->num_strings) {
+        fprintf(stderr, "Requested string id: %d not found, return dummy string\n", id);
+        return dummy_string_res;
+    }
     return pstrres->strings[id];
 }
 
@@ -346,15 +450,51 @@ void __stdcall gos_DeleteFont( HGOSFONT3D Fonthandle )
 
 DWORD __stdcall gos_NewEmptyTexture( gos_TextureFormat Format, const char* Name, DWORD HeightWidth, DWORD Hints/*=0*/, gos_RebuildFunction pFunc/*=0*/, void *pInstance/*=0*/)
 {
+    gosASSERT(0 && "Not implemented");
     return -1;
 }
 DWORD __stdcall gos_NewTextureFromMemory( gos_TextureFormat Format, const char* FileName, BYTE* pBitmap, DWORD Size, DWORD Hints/*=0*/, gos_RebuildFunction pFunc/*=0*/, void *pInstance/*=0*/)
 {
-    return -1;
+    //gosASSERT(0 && "Not implemented");
+
+    gosTexture* ptex = new gosTexture(Format, FileName, Hints, pBitmap, Size);
+    return g_renderer.addTexture(ptex);
 }
 void __stdcall gos_DestroyTexture( DWORD Handle )
 {
+    //gosASSERT(0 && "Not implemented");
+    g_renderer.deleteTexture(Handle);
 }
+
+void __stdcall gos_LockTexture( DWORD Handle, DWORD MipMapSize, bool ReadOnly, TEXTUREPTR* TextureInfo )
+{
+    // not implemented yet
+    gosASSERT(MipMapSize == 0);
+    int mip_level = 0; //func(MipMapSize);
+
+    gosTextureInfo info;
+    int pitch = 0;
+    gosTexture* ptex = g_renderer.getTexture(Handle);
+    ptex->getTextureInfo(&info);
+    BYTE* pdata = ptex->Lock(mip_level, ReadOnly, &pitch);
+
+    TextureInfo->pTexture = (DWORD*)pdata;
+    TextureInfo->Width = info.width_;
+    TextureInfo->Height = info.height_;
+    TextureInfo->Pitch = pitch;
+    TextureInfo->Type = info.format_;
+
+    //gosASSERT(0 && "Not implemented");
+}
+
+void __stdcall gos_UnLockTexture( DWORD Handle )
+{
+    gosTexture* ptex = g_renderer.getTexture(Handle);
+    ptex->Unlock();
+
+    //gosASSERT(0 && "Not implemented");
+}
+
 void __stdcall gos_PushRenderStates()
 {
 }
@@ -402,14 +542,6 @@ void __stdcall gos_TextSetRegion( int Left, int Top, int Right, int Bottom )
 }
 
 void __stdcall gos_TextStringLength( DWORD* Width, DWORD* Height, const char *Message, ... )
-{
-}
-
-void __stdcall gos_LockTexture( DWORD Handle, DWORD MipMapSize, bool ReadOnly, TEXTUREPTR* TextureInfo )
-{
-}
-
-void __stdcall gos_UnLockTexture( DWORD Handle )
 {
 }
 
