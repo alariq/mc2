3984251a468d43d90bd76b47f1b139b3f8ee6b19
sebi <alariq@gmail.com>
2016-10-27 00:46:34 +0300

    font load function and structs

---
+++ GameOS/gameos/gos_font.cpp
@@ -0,0 +1,41 @@
+#include<inttypes.h>
+#include <stdio.h>
+// errno + strerror
+#include <errno.h>
+#include <string.h>
+
+#include "gameos.hpp"
+#include "gos_font.h"
+
+bool gos_load_glyphs(const char* glyphFile, gosGlyphInfo& gi)
+{
+    FILE* glyph_info = fopen(glyphFile, "rb");
+    if(!glyph_info) {
+        int last_err = errno;
+        SPEW(("fopen: %s\n", strerror(errno)));
+        return false;
+    }
+
+    fread(&gi.num_glyphs_, sizeof(gi.num_glyphs_), 1, glyph_info);
+    fread(&gi.start_glyph_, sizeof(gi.start_glyph_), 1, glyph_info);
+
+    fread(&gi.max_advance_, sizeof(gi.max_advance_), 1, glyph_info);
+    fread(&gi.font_line_skip_, sizeof(gi.font_line_skip_), 1, glyph_info);
+
+    size_t num_structs_read = 0;
+
+    gi.glyphs_ = new gosGlyphMetrics[gi.num_glyphs_];
+
+    while(num_structs_read!= gi.num_glyphs_) {
+        num_structs_read+= fread(&gi.glyphs_[num_structs_read], 
+                sizeof(gosGlyphMetrics), 
+                gi.num_glyphs_ - num_structs_read,
+                glyph_info);
+    }
+
+    fclose(glyph_info);
+
+    return true;
+}
+
+
---
+++ GameOS/gameos/gos_font.h
@@ -0,0 +1,25 @@
+#ifndef GOS_FONT_H
+#define GOS_FONT_H
+
+typedef struct {
+    int32_t minx;
+    int32_t maxx;
+    int32_t miny;
+    int32_t maxy;
+    int32_t advance;
+    uint32_t valid;
+    uint32_t u;
+    uint32_t v;
+} gosGlyphMetrics;
+
+struct gosGlyphInfo {
+    uint32_t num_glyphs_;
+    uint32_t start_glyph_;
+    gosGlyphMetrics* glyphs_;
+    uint32_t max_advance_;
+    uint32_t font_line_skip_;
+};
+
+bool gos_load_glyphs(const char* glyphFile, gosGlyphInfo& gi);
+
+#endif // GOS_FONT_H
